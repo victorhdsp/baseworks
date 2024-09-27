8c96431d4dbd1f4bcb3b410fc2a981ba75012df0
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-27 12:44:22 -0300

    paginação

---
+++ src/components/Pokedex/PokedexPagination.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="pokedex-pagination">
+        <el-pagination :total="pokedex.total" :page-size="pokedex.size" :current-page="pokedex.current"
+            @current-change="onPageChange" background layout="prev, pager, next" />
+    </section>
+</template>
+
+<script setup lang="ts">
+import { ElPagination } from 'element-plus';
+import { usePokedexPaginationStore } from '@/stores/pokedexPagination';
+const pokedex = usePokedexPaginationStore();
+
+const onPageChange = (page: number) => {
+    pokedex.setCurrent(page);
+};
+</script>
+
+<style lang="scss" scoped>
+#pokedex-pagination {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .list {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 1rem;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
     <PokemonList />
+    <PokemonPagination />
   </main>
 </template>
 
 <script setup lang="ts">
-import PokemonList from '@/components/Pokedex/PokedexList.vue'
+import PokemonList from '@/components/Pokedex/PokedexList.vue';
+import PokemonPagination from '@/components/Pokedex/PokedexPagination.vue';
 </script>
