41bc078c09e8547718f29be769bfa404b9e57a39
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-28 15:28:58 -0300

    pagina

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,47 +1,50 @@
 <template>
-    <PokemonPageHeader name="Articuno" :index="2" />
-    <main>
-        <div v-if="!pokemon">
-            Carregando...
-        </div>
-        <section v-else id="pokemon-view">
+    <div v-if="!pokemon">
+        Carregando...
+    </div>
+    <main v-else>
+        <PokemonPageHeader :name="pokemon.name" :index="pokemon.id" />
+        <section id="pokemon-view">
             <PokemonImage :index="pokemon.id" :alt="pokemon.name" />
-            <div class="pokemon-caracteristics"></div>
-            <div class="pokemon-abilities"></div>
-            <div class="pokemon-stats"></div>
-            <div class="pokemon-types"></div>
-            <div class="pokemon-weakness"></div>
-            <div class="pokemon-description"></div>
-            <div class="pokemon-evolutions"></div>
+            <PokemonTypes :types="pokemon.types" />
+            <PokemonStats :stats="pokemon.stats" />
+            <PokemonEvolutions class="evolutions" :evolutions="pokemon.evolution" />
         </section>
     </main>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import type { IPokemon } from '@/lib/types/api';
+import type { IPokemon } from '@/lib/types/pokemon';
 import PokemonPageHeader from '@/components/organism/PokemonPageHeader/index.vue';
 import PokemonImage from '@/components/atom/PokemonImage/index.vue';
+import PokemonStats from '@/components/organism/PokemonStats/index.vue';
+import PokemonTypes from '@/components/organism/PokemonTypes/index.vue';
+import PokemonEvolutions from '@/components/organism/PokemonEvolutions/index.vue';
 import api from '@/lib/api';
+import router from '@/router';
 
 const pokemon = ref<IPokemon | null>(null);
+const page = router.currentRoute.value.params.id;
 
 onMounted(async () => {
-    const data = await api.getUnique('1');
-    pokemon.value = data;
+    if (page) {
+        const data = await api.getUnique(`${page}`);
+        pokemon.value = data;
+    } else {
+        router.push('/');
+    }
 });
 </script>
 
 <style lang="scss" scoped>
 #pokemon-view {
     @apply grid grid-cols-2 gap-4;
-    grid-template-rows: repeat(4, minmax(0, 1fr));
+    grid-template-rows: repeat(3, minmax(0, 1fr));
+    @apply container;
 
-    .pokemon-types,
-    .pokemon-weakness,
-    .pokemon-description,
-    .pokemon-evolutions {
-        @apply col-start-2;
+    .evolutions {
+        @apply col-start-2 row-start-1 row-span-3;
     }
 }
 </style>
