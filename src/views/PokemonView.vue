05f62992429c47d40424eecee22b4e09617112e5
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-28 10:24:57 -0300

    criação da pagina de pokemon

---
+++ src/views/PokemonView.vue
@@ -0,0 +1,10 @@
+<template>
+    <PokemonPageHeader />
+    <h1>Pokemon</h1>
+</template>
+
+<script setup lang="ts">
+import PokemonPageHeader from '@/components/organism/PokemonPageHeader/index.vue';
+</script>
+
+<style lang="scss" scoped></style>
