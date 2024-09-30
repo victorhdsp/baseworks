f49f82be4b30494802ca0b8bf257477383aa2b52
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-30 09:04:30 -0300

    animacoes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <Header />
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <transition>
+      <component :is="Component" />
+    </transition>
+  </RouterView>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Header from '@/components/organism/PageHeader/index.vue'
 </script>
+
+<style lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
