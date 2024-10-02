4e0d25a3dc3ef10fa2fc8fe9a079635a10bea957
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-10-02 18:27:09 +0000

    rotas assincronas

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import api from '@/lib/api'
+
+const prefetchPokemonsRoutes = async () => {
+  try {
+    const data = await api.getAll({offset:0, limit:1302});
+    return data.results.map((pokemon) => ({
+      path: `/pokemon/${pokemon.id}`,
+      name: pokemon.name,
+      component: () => import('../views/PokemonView.vue'),
+      meta: { transition: 'slide-right' }
+    }))
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +39,12 @@ const router = createRouter({
       meta: { transition: 'slide-right' }
     }
   ]
+})
+
+prefetchPokemonsRoutes().then((routes) => {
+  routes?.forEach((route) => {
+    router.addRoute(route)
+  })
 })
 
 export default router
