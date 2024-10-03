7a8489d07b343734a808c6b3769dabe9ca03374b
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-10-03 23:28:07 +0000

    vercel json

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import api from '@/lib/api'
-
-const prefetchPokemonsRoutes = async () => {
-  try {
-    const data = await api.getAll({offset:0, limit:1302});
-    return data.results.map((pokemon) => ({
-      path: `/pokemon/${pokemon.id}`,
-      name: pokemon.name,
-      component: () => import('../views/PokemonView.vue'),
-      meta: { transition: 'slide-right' }
-    }))
-  } catch (error) {
-    console.log(error);
-    return;
-  }
-}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,18 +17,12 @@ const router = createRouter({
       meta: { transition: 'slide-left' }
     },
     {
-      path: '/pokemon/:id?',
+      path: '/pokemon/:id',
       name: 'Pokemon',
       component: () => import('../views/PokemonView.vue'),
       meta: { transition: 'slide-right' }
     }
   ]
-})
-
-prefetchPokemonsRoutes().then((routes) => {
-  routes?.forEach((route) => {
-    router.addRoute(route)
-  })
 })
 
 export default router
